3033a1ff66578a1950b25212c3894c6556ffe4f3
Davide-Morellato <morellato.davide@yahoo.it>
2024-09-26 17:34:28 +0200

    ADD: routes + route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
 
-import Header from './components/header/header.component'; //importo l'header
-import Homepage from './pages/homepage.component'; //importo la homepage
+import Header from "./components/header/header.component"; //importo l'header
+import Homepage from "./pages/homepage.component"; //importo la homepage
+import { Routes, Route } from "react-router-dom"; //importo le rotte
 
 function App() {
   return (
@@ -9,8 +10,12 @@ function App() {
       {/* inserisco il component header */}
       <Header />
 
+      {/* inserisco il componente Routes, al cui interno inserisco la singola rotta Route con il percorso (path) e l'elemento da dover visualizzare (element={componente})*/}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
       {/* inserisco il componente homepage */}
-      <Homepage />
+      {/* <Homepage /> */}
     </div>
   );
 }

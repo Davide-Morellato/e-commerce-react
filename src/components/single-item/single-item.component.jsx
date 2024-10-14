95f8b47e95b62ea64cd8bf8f5b0b1fffbd1309e7
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-14 17:08:31 +0200

    ADD: single-item component (all static, an example)

---
+++ src/components/single-item/single-item.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './single-item.styles.scss';
+
+
+//recupero dalle props, le proprietà dei prodotti inseriti nel carrello + la quantità 
+const SingleItem = ({image, name, price, quantity}) => {
+    return(
+        <div className="single-item">
+            <img src="./items_images/smartphones/smatphone_1.jpg" alt="single_item" />
+            <div className="details">
+                <span className="name">
+                    Smartphone
+                </span>
+                <span className="price">
+                    1 x 20€
+                </span>
+            </div>
+        </div>
+    )
+};
+
+export default SingleItem

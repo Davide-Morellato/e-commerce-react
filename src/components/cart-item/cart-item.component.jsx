984033aeaac8f25a967e61ac925a3ea81902cbb3
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-14 17:49:21 +0200

    ADD: cart-item component

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './cart-item.styles.scss'
+
+const CartItem = () => {
+    return(
+        <div className="cart-item">
+            <div className="remove_button"> - </div>
+            <div className="img_container">
+                <img src="./items_images/smartphones/smatphone_1.jpg" alt="" />
+            </div>
+            <span className="name">Product</span>
+            <span className="price">xxx €</span>
+            <span className="quantity_container">
+                <span className="arrow"> ← </span>
+                <span className="quantity"> X </span>
+                <span className="arrow"> → </span>
+            </span>
+        </div>
+    )
+}
+
+export default CartItem

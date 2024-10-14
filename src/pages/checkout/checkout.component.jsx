a2adb5dc9601e46ff0fe12c312002bc2e0a8d29c
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-14 17:13:42 +0200

    ADD: dispatch to checkout component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import './checkout.styles.scss'
+import { useDispatch } from "react-redux";
+import { toggleCart } from "../../redux/cart/cart.actions"; //importo la funzione per il controllo dell'apertura/chiusura del carrello
 
 const Checkout = () => {
+
+    //salvo l'HOOK per il recupero delle funzioni
+    const dispatch = useDispatch();
+
+    //richiamo la funzione toggleCart grazie al dispatch
+    useEffect(() => {
+        dispatch(toggleCart())
+    }, [dispatch])
+
+
     return(
-        <div className="checkout">CASSA</div>
+        <div className="checkout">
+        <h1>Carrello Utente</h1>
+        <div className="header_checkout">
+            <div className="data_cart">
+                <span></span>
+            </div>
+            <div className="data_cart">
+                <span>Product</span>
+            </div>
+            <div className="data_cart">
+                <span>Description</span>
+            </div>
+            <div className="data_cart">
+                <span>Price</span>
+            </div>
+            <div className="data_cart">
+                <span>Quantity</span>
+            </div>
+            <div className="total">TOTAL: xxx€</div>
+        </div>
+        </div>
     )
 }
 

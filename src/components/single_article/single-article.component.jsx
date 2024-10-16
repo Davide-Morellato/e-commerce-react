1383c15b58805db3648030d5ee51ed9e61711208
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-16 11:26:57 +0200

    ADD: dispatch for addProduct to button into single-article

--- src/components/single_article/single-article.component.jsx
+++ src/components/single_article/single-article.component.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import './single-article.styles.scss';
+import { useDispatch } from "react-redux";
+import addProduct from '../../redux/cart/cart.utilities'
 
 //aggiungo un parametro per recuperare le imamgini dei prodotti da categories [documento salvato in firestore]
-const Article = ({imageUrl}) => {
+const Article = (article) => {
+
+    const {imageUrl} = article;
+
+    const dispatch = useDispatch();
+
     return (
         //imposto come sfondo della card l'immagine recuperata da categories
         <div className="article" style={{backgroundImage: `url(${imageUrl})`}}>
             <div className="add-to-cart">
-                <button type="button">ADD TO CART</button>
+                <button type="button" onClick={() => dispatch(addProduct(article))}>ADD TO CART</button>
             </div>
         </div>
     )
 };
 
 export default Article;
+
+
+
+<div className=""></div>

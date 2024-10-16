794b0c7ff14df5c478d9d5cb9337f9492ecdd6c3
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-16 10:18:06 +0200

    ADD: ADD_PRODUCT; REMOVE_PRODUCT; DELETE_PRODUCT; EMPTY_CART to cart.types.js

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,8 +1,21 @@
 //
 //definisco il tipo di azione per il cart_dropdown_menu (se visibile o nascosto)
 export const CartActionTypes = {
+    //definisco il tipo di azione per l'apertura e chiusura del dropdown_menu
     TOGGLE_PREVIEW: 'TOGGLE_PREVIEW',
 
     //definisco il tipo di azione per fare in modo che il dropdown_menu si chiuda quando si accede al checkout
-    CLOSE_CART: 'CLOSE_CART'
+    CLOSE_CART: 'CLOSE_CART',
+
+    //definisco il tipo di azione per fare in modo di aumentare (+1) la quantità di un singolo prodotto nel dropdown_menu
+    ADD_PRODUCT: 'ADD_PRODUCT',
+
+    //definisco il tipo di azione per fare in modo di diminuire (-1) la quantità di un singolo prodotto nel dropdown_menu
+    REMOVE_PRODUCT: 'REMOVE_PRODUCT',
+
+    //definisco il tipo di azione per fare in modo da eliminare un singolo prodotto nel dropdown_menu
+    DELETE_PRODUCT: 'DELETE_PRODUCT',
+
+    //definisco il tipo di azione per fare in modo di dsvuotare il carrello
+    EMPTY_CART: 'EMPTY_CART'
 }

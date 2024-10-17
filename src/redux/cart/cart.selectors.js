1827c8fe62c39151cafe5851c1e7b5814bf385a0
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 10:40:12 +0200

    ADD: cart.selector.js to create selector for cart products

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect"; //importo l'HOOK di creazione selettori
+
+//dichiaro una variabile in cui salvo i dati del carrello recuperati dallo state del reducer
+export const selectCart = state => state.cart;
+
+//dichiaro una variabile in cui creo il selettore ponendo due parametri:
+//I -> la dipendanza da cui prendere i dati (in quanto collegato allo state del carrello)
+//II -> callback per prendere i dettagli dei prodotti
+export const selectCartProducts = createSelector(
+    [selectCart],
+    products => products.cartItems
+)
+
+//dichiaro una variabile in cui creo il selettore per contare i prodotti nel carrello
+export const selectCountProducts = createSelector(
+    [selectCartProducts], //dipendenza da cui recupero i dati
+
+    //callback in cui con il metodo reduce() riesco ad ottenere la quantità totale dei prodotti
+    //questo metodo itera e "riduce" i valori di un array in un unico valore
+    //assegno due parametri:
+    //I -> CALLBACK composta a sua volta da 2 parametri: 
+            //I -> accumulatore [countQuantity] 
+            //II -> elemento dell'array [product]
+    //
+    //II -> VALORE INIZIALE
+    // 
+    products => products.reduce(
+        (countQuantity, product) => countQuantity + product.quantity, //callback con ritorno di una condizione funzionale
+        0 //----------------------------------------------------------->valore iniziale
+    )
+)

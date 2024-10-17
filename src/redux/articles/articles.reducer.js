46ff44899c41f5db22a653868169eecc8fffa7ac
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 13:23:23 +0200

    ADD: "case" for SUCCESS and FAIL

--- src/redux/articles/articles.reducer.js
+++ src/redux/articles/articles.reducer.js
@@ -2,17 +2,33 @@ import { ArticlesActionTypes } from "./articles.types";
 
 //definisco lo state iniziale
 const INITIAL_STATE = {
-    products: null
+    products: null,
+    isLoading: false,
+    error: null
 }
 
 //controllo l'azione da lanciare in base alla tipologia di azione
 //in base al tipo di cambiamento aggiungo allo state il payload (recuperato da articles.actions.js)
 const ArticlesReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case ArticlesActionTypes.GET_ARTICLES:
+        case ArticlesActionTypes.GET_ARTICLES_START:
             return{
                 ...state,
+                isLoading: true
+            }
+        
+        case ArticlesActionTypes.GET_ARTICLES_SUCCESS:
+            return{
+                ...state,
+                isLoading: false,
                 products: action.payload
+            }
+
+        case ArticlesActionTypes.GET_ARTICLES_FAIL:
+            return{
+                ...state,
+                isLoading: false,
+                error: action.payload
             }
         default:
             return state

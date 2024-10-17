15939a2b77218f3c5a1dbc1330258b3f2ddb2d5f
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 10:49:38 +0200

    ADD: selector functions

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import './cart-icon.styles.scss';
 
-import { useDispatch } from "react-redux"; //importo il dispatch per il recupero della funzione
+import { useSelector, useDispatch } from "react-redux"; //importo il dispatch per il recupero della funzione & il selector per i selettori
 import { toggleCart } from "../../redux/cart/cart.actions"; //importo la funzione per il controllo dell'apertura/chiusura del carrello
 import ShoppingCartItem from "../../assets/img/shopping-cart.png"; //importo l'icona del carrello
+import { selectCountProducts } from "../../redux/cart/cart.selectors"; //importo il selettore per il conteggio dei prodotti
 
 
 const CartIcon = () => {
@@ -11,10 +12,17 @@ const CartIcon = () => {
     //dichiaro il dispatch per il recupero della funzione toggleCart
     const dispatch = useDispatch();
 
+    //dichiaro una variabile in cui recupero il selettore per il conteggio prodotti
+    const numProducts = useSelector(selectCountProducts)
+
     return(
+        //aggiungo la direttiva onClick che, tramite una callback, mi permette di gestire l'apertura e la chiusura del dropdown, grazie al dispatch che recupera la funzione toggleCart()
         <div className="cart" onClick={() => dispatch(toggleCart())}>
           <img src={ShoppingCartItem} className="cart_icon" alt="cart" />
-          <span className="item_counter">0</span>
+
+          {/* inserisco la variabile che dal selettore recupera il totale dei prodotti nel carrello */}
+          <span className="item_counter"> {numProducts} </span> 
+          
         </div>
     )
 };
--- src/components/dropdown-cart/dropdown-cart.component.jsx
+++ src/components/dropdown-cart/dropdown-cart.component.jsx
@@ -3,17 +3,30 @@ import { useNavigate } from "react-router-dom"; //per la navigazione tra pagine
 import SingleItem from '../single-item/single-item.component' //importo il componente SingleItem
 import './dropdown-cart.styles.scss';
 import { Button } from "../../styled-components/button"; //importo lo style-components
+import { useSelector } from "react-redux"; //importo il selettore
+import { selectCartProducts } from "../../redux/cart/cart.selectors"; //importo la funzione per il selettore dei prodotti nel carrello
 
 const DropdownCart = () => {
 
     //salvo in una variabile l'HOOK useNavigate
     const navigate = useNavigate();
 
+    const products = useSelector(selectCartProducts)
+
     return(
         <div className="dropdown-cart">
             <div className="cart_items">
-                <SingleItem />
-              {/* <span className="empty_mex"> CARRELLO VUOTO </span> */}
+                {
+                    products.length > 0
+                    ?
+                    products.map(singleProduct => {
+                        <SingleItem key={singleProduct.id}{...singleProduct}/>
+                    })
+                    :
+                    (
+                        <span className="empty_mex"> CARRELLO VUOTO </span>
+                    )
+                }
             </div>
             {/* aggiungo l'evento al click di navigazione per il checkout
                 NB -> /checkout dichiarata in App.js*/}

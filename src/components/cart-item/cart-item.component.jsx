4e167149bc0423c4b6e59934ea3b6f518573af84
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 11:29:23 +0200

    ADD: params to CartItem

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import './cart-item.styles.scss'
 
-const CartItem = () => {
+//aggiungo il parametro product
+const CartItem = (product) => {
+
+    //salvo nel parametro product i valori dei prodotti
+    const {imageUrl, name, price, quantity} = product
+
     return(
         <div className="cart-item">
             <div className="remove_button"> - </div>
             <div className="img_container">
-                <img src="./items_images/smartphones/smartphone_1.jpg" alt="" />
+                <img src={imageUrl} alt="" />
             </div>
-            <span className="name">Product</span>
-            <span className="price">xxx €</span>
+            <span className="name">{name}</span>
+            <span className="price">{price} €</span>
             <span className="quantity_container">
                 <span className="arrow"> ← </span>
-                <span className="quantity"> X </span>
+                <span className="quantity"> {quantity} </span>
                 <span className="arrow"> → </span>
             </span>
         </div>

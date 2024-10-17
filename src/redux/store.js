29ce477ffdc4607b00efd9579906949088769800
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 12:20:44 +0200

    ADD: persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux'; //importo la funzione createStore da REDUX 
 
 import { logger } from 'redux-logger'; //importo il logger di REDUX che funge da middleware per il monitoraggio dello State
+import { persistStore } from 'redux-persist/es/persistStore'; //importo il persistStore di REDUX per la persistenza dei dati
 
 import {rootReducer} from './root.reducer'; //importo il reducer
 
@@ -15,3 +16,5 @@ const middlewares = [
 //assegnandogli come parametro il reducer
 //e anche l'applyMiddleware, a cui assegno come parametro l'array per tutti i middlewares (con spread operator perché ho necessità di tutti i valori)
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+export const storePersistance = persistStore(store);

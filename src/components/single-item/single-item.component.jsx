e9afbbe4f8b0ca10ac2ac0dd0a8732a3f8c4f74e
Davide-Morellato <morellato.davide@yahoo.it>
2024-10-17 10:51:00 +0200

    ADD: real data into SingleItem

--- src/components/single-item/single-item.component.jsx
+++ src/components/single-item/single-item.component.jsx
@@ -4,16 +4,16 @@ import './single-item.styles.scss';
 
 
 //recupero dalle props, le proprietà dei prodotti inseriti nel carrello + la quantità 
-const SingleItem = ({image, name, price, quantity}) => {
+const SingleItem = ({imageUrl, name, price, quantity}) => {
     return(
         <div className="single-item">
-            <img src="./items_images/smartphones/smartphone_1.jpg" alt="single_item" />
+            <img src={imageUrl} alt="single_item" />
             <div className="details">
                 <span className="name">
-                    Smartphone
+                    {name}
                 </span>
                 <span className="price">
-                    1 x 20€
+                    {quantity} x {price} €
                 </span>
             </div>
         </div>
